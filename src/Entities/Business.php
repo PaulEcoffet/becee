5d38f00cf85e5a67e142d30729f028c2c3c4a8d4
Mathieu-Seurin <matseurin@cegetel.net>
2014-04-14 15:40:06 +0200

    Creation of Business Class

---
+++ src/Entities/Business.php
@@ -0,0 +1,26 @@
+<?php
+
+class Business
+{
+
+	public $name = NULL;
+	public $id = NULL; 
+	public $longitude = NULL;
+	public $latitude = NULL;
+	public $manager = NULL;
+	public $website = NULL;
+	public $tags = NULL;
+	//public $images = NULL;
+	public $visits = NULL;
+	public $comments = NULL;
+	public $features = NULL;
+
+
+
+
+	public function __construct()
+	{
+
+	}
+
+}
