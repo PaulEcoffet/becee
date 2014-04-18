8557e119871df070f93a0849c2b62adf38c21d6e
NestarZ <elias.rouzlane@gmail.com>
2014-04-19 00:46:41 +0200

    Updated SQL Tables (Fix)

--- app/db_config.sql
+++ app/db_config.sql
@@ -7,6 +7,7 @@ CREATE TABLE businesses(
     lng FLOAT(10, 6),
     manager_id INT UNSIGNED,
     website VARCHAR(255),
+    FOREIGN KEY (manager_id) REFERENCES users(id),
     PRIMARY KEY (id));
 
 CREATE TABLE business_tags(
@@ -20,6 +21,8 @@ CREATE TABLE business_images(
     business_id INT UNSIGNED,
     order TINYINT UNSIGNED,
     path VARCHAR(40),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (business_id) REFERENCES businesses(id),
     PRIMARY KEY (id));
 
 CREATE TABLE business_visits(
@@ -27,25 +30,30 @@ CREATE TABLE business_visits(
     user_id INT UNSIGNED,
     business_id INT UNSIGNED,
     visit_date DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (business_id) REFERENCES businesses(id),
     PRIMARY KEY (id));
 
 CREATE TABLE business_comments(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED,
-    business_visit_id INT UNSIGNED,
+    business_id INT UNSIGNED,
     comment TEXT,
     pub_date DATETIME,
     status TINYINT UNSIGNED,
     vote_pos SMALLINT UNSIGNED,
     vote_neg SMALLINT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (business_id) REFERENCES businesses(id),
     PRIMARY KEY (id));
 
 CREATE TABLE link_business_tags(
-    id_business INT UNSIGNED,
-    id_business_tag INT UNSIGNED,
+    business_id INT UNSIGNED,
+    tag_id INT UNSIGNED,
     nb_yes SMALLINT UNSIGNED,
     nb_no SMALLINT UNSIGNED,
-    PRIMARY KEY (id_business, id_business_tag));
+    FOREIGN KEY (tag_id) REFERENCES business_tags(id),
+    FOREIGN KEY (business_id) REFERENCES businesses(id));
 
 CREATE TABLE business_features(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -53,33 +61,42 @@ CREATE TABLE business_features(
     PRIMARY KEY (id));
 
 CREATE TABLE link_features_tags(
-    id_feature INT UNSIGNED,
-    id_feature_tag INT UNSIGNED,
+    feature_id INT UNSIGNED,
+    tag_id INT UNSIGNED,
     pertinence DOUBLE,
-    PRIMARY KEY (id_feature, id_feature_tag));
+    FOREIGN KEY (tag_id) REFERENCES business_tags(id),
+    FOREIGN KEY (feature_id) REFERENCES business_features(id));
 
 CREATE TABLE score_businesses_features(
-    id_business INT UNSIGNED,
-    id_feature INT UNSIGNED,
+    business_id INT UNSIGNED,
+    feature_id INT UNSIGNED,
     elo_score INT UNSIGNED,
-    PRIMARY KEY (id_business, id_feature));
+    FOREIGN KEY (feature_id) REFERENCES business_tags(id),
+    FOREIGN KEY (business_id) REFERENCES businesses(id));
 
 CREATE TABLE businesses_comparaisons(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    id_business_visit1 INT UNSIGNED,
-    id_business_visit2 INT UNSIGNED,
+    business_visit1_id INT UNSIGNED,
+    business_visit2_id INT UNSIGNED,
     winner TINYINT,
+    feature_id INT UNSIGNED,
     trustability DOUBLE,
+    FOREIGN KEY (business_visit1_id) REFERENCES businesses(id));
+    FOREIGN KEY (business_visit2_id) REFERENCES businesses(id));
+    FOREIGN KEY (feature_id) REFERENCES business_features(id));
     PRIMARY KEY (id));
 
 -- USERS TABLES
 
 CREATE TABLE users(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
     name VARCHAR(40) NOT NULL,
     hashed_password VARCHAR(255),
+    category INT UNSIGNED,
     salt VARCHAR(255),
     trustability DOUBLE,
+    FOREIGN KEY (category) REFERENCES user_categories(id));
     PRIMARY KEY (id));
 
 CREATE TABLE user_categories(
