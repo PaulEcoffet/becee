b11da3a196efa277eb6627a96044017572743b21
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-04-22 16:49:18 +0200

    Some tests for the new route parameters feature

--- src/Controllers/Dummy.php
+++ src/Controllers/Dummy.php
@@ -6,6 +6,6 @@ class Dummy
 {
     public function viewAction($request)
     {
-        return $request->parseTemplate('test.html.twig', array('nb' => 2));
+        return $request->parseTemplate('test.html.twig', array('nb' => $request->getParamsUri('nb')));;
     }
 }
