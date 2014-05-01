9acbf036a5b8e4f974087b96abd9be53df285e03
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-05-01 22:32:04 +0200

    Test for the new RedirectResponse

--- src/Controllers/Dummy.php
+++ src/Controllers/Dummy.php
@@ -7,6 +7,14 @@ class Dummy
     public function viewAction($request)
     {
         $filesManager = $request->getManager('Files');
-        return new \QDE\Responses\TwigResponse('test.html.twig', array('nb' => $request->getParamsUri('nb')));
+        $nb = (int) $request->getParamsUri('nb');
+        if($nb > 0)
+        {
+            return new \QDE\Responses\TwigResponse('test.html.twig', array('nb' => $nb));
+        }
+        else
+        {
+            return new \QDE\Responses\RedirectResponse('home');
+        }
     }
 }
