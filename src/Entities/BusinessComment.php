b13bba0db108301b8abb4b43a50e62572f096030
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-05-07 22:10:28 +0200

    Creation of the BusinessComment Entity

---
+++ src/Entities/BusinessComment.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace Becee\Entities;
+
+class BusinessComment
+{
+    protected $comment;
+    protected $userName;
+    protected $userId;
+    protected $userAvatar;
+    protected $pubDate;
+    protected $votePos;
+    protected $voteNeg;
+    protected $id;
+
+
+    public function __construct($comment='', $user_name='Anonymous', $user_id=0, $user_avatar=null, $pub_date=0, $vote_pos=0, $vote_neg=0, $id='')
+    {
+        $this->setComment($comment);
+        $this->setUserName($user_name);
+        $this->setUserId($user_id);
+        $this->setUserAvatar($user_avatar);
+        $this->setPubDate($pub_date);
+        $this->setVotePos($vote_pos);
+        $this->setVoteNeg($vote_neg);
+        $this->setId($id);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach ($data as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function getTotalVotes()
+    {
+        return $this->getVotePos() + $this->getVoteNeg();
+    }
+
+    /**
+     * Get comment.
+     *
+     * @return comment.
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set comment.
+     *
+     * @param comment the value to set.
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get userName.
+     *
+     * @return userName.
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * Set userName.
+     *
+     * @param userName the value to set.
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    /**
+     * Get userId.
+     *
+     * @return userId.
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set userId.
+     *
+     * @param userId the value to set.
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * Get userAvatar.
+     *
+     * @return userAvatar.
+     */
+    public function getUserAvatar()
+    {
+        return $this->userAvatar;
+    }
+
+    /**
+     * Set userAvatar.
+     *
+     * @param userAvatar the value to set.
+     */
+    public function setUserAvatar($userAvatar)
+    {
+        $this->userAvatar = $userAvatar;
+    }
+
+    /**
+     * Get pubDate.
+     *
+     * @return pubDate.
+     */
+    public function getPubDate()
+    {
+        return $this->pubDate;
+    }
+
+    /**
+     * Set pubDate.
+     *
+     * @param pubDate the value to set.
+     */
+    public function setPubDate($pubDate)
+    {
+        $this->pubDate = $pubDate;
+    }
+
+    /**
+     * Get votePos.
+     *
+     * @return votePos.
+     */
+    public function getVotePos()
+    {
+        return $this->votePos;
+    }
+
+    /**
+     * Set votePos.
+     *
+     * @param votePos the value to set.
+     */
+    public function setVotePos($votePos)
+    {
+        if($this->votePos !== null)
+        {
+            $this->votePos = $votePos;
+        }
+        else
+        {
+            $this->votePos = 0;
+        }
+    }
+
+    /**
+     * Get voteNeg.
+     *
+     * @return voteNeg.
+     */
+    public function getVoteNeg()
+    {
+        return $this->voteNeg;
+    }
+
+    /**
+     * Set voteNeg.
+     *
+     * @param voteNeg the value to set.
+     */
+    public function setVoteNeg($voteNeg)
+    {
+        if($this->voteNeg !== null)
+        {
+            $this->voteNeg = $voteNeg;
+        }
+        else
+        {
+            $this->voteNeg = 0;
+        }
+    }
+
+     /**
+      * Get id.
+      *
+      * @return id.
+      */
+     public function getId()
+     {
+         return $this->id;
+     }
+
+    /**
+     * Set id.
+     *
+     * @param id the value to set.
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
