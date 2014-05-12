5e0b17f5ae7859ff963b191fd0adc9c472fe3b8a
Paul Ecoffet <ecoffet.paul@gmail.com>
2014-05-12 15:04:15 +0200

    Db config removing useless table -- WILL DROP YOUR DATABASE

--- app/db_config.sql
+++ app/db_config.sql
@@ -92,7 +92,7 @@ CREATE TABLE `business_comments` (
   KEY `business_id` (`business_id`),
   CONSTRAINT `business_comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `business_comments_ibfk_2` FOREIGN KEY (`business_id`) REFERENCES `businesses` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +101,7 @@ CREATE TABLE `business_comments` (
 
 LOCK TABLES `business_comments` WRITE;
 /*!40000 ALTER TABLE `business_comments` DISABLE KEYS */;
-INSERT INTO `business_comments` VALUES (5,1,2,'Ultra cool, gg','2014-05-02 10:40:05',NULL,0,0),(6,2,2,'Super, cimer','2014-05-02 10:40:06',NULL,0,0),(7,2,1,'Ambiance de malade, (y)','2014-05-02 10:40:05',NULL,0,0),(8,3,2,'Un gros pouce vert/bleu','2014-05-02 10:40:05',NULL,0,0);
+INSERT INTO `business_comments` VALUES (5,1,2,'Ultra cool, gg','2014-05-02 10:40:05',NULL,0,0),(6,2,2,'Super, cimer','2014-05-02 10:40:06',NULL,0,0),(7,2,1,'Ambiance de malade, (y)','2014-05-02 10:40:05',NULL,0,0),(8,3,2,'Un gros pouce vert/bleu','2014-05-02 10:40:05',NULL,0,0),(10,4,2,'C\'est merdique!','2014-05-12 14:29:20',0,0,0),(11,4,4,'Pas mal','2014-05-12 14:29:47',0,0,0);
 /*!40000 ALTER TABLE `business_comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -249,6 +249,39 @@ LOCK TABLES `businesses` WRITE;
 INSERT INTO `businesses` VALUES (1,'The Black Pearl','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',1,NULL,NULL,'06 65 58 85 33',NULL,3),(2,'Heiki','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',1,NULL,NULL,'05 19 08 85 13',NULL,4),(3,'Getsumen','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',1,NULL,NULL,'07 56 89 85 22',NULL,NULL),(4,'Thorion','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',1,NULL,NULL,'05 89 08 12 13',NULL,5),(5,'Restaurant Universitaire Cap U','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',2,NULL,NULL,'08 19 11 85 13',NULL,4),(6,'The Green Pearl','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',2,NULL,NULL,'01 58 10 44 03',NULL,3),(7,'The Yellow Pearl','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',3,NULL,NULL,'02 19 18 85 03',NULL,2),(8,'The Blue Pearl','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',3,NULL,NULL,'05 11 08 85 13',NULL,5),(9,'Valkar','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',3,NULL,NULL,'03 11 11 81 13',NULL,4),(10,'Les Cerisiers en Fleur','Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',3,NULL,NULL,'09 19 88 00 00',NULL,5);
 /*!40000 ALTER TABLE `businesses` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`becee`@`localhost`*/ /*!50003 TRIGGER `businesses_AINS` AFTER INSERT ON `businesses` FOR EACH ROW
+BEGIN
+INSERT IGNORE INTO vm_score_businesses_features (business_id, feature_id, elo_score, rank_zone, last_computation_date)
+ SELECT DISTINCT
+	businesses.id as business_id,
+	features.id as feature_id,
+	1400 as elo_score,
+	1000 as rank_zone,
+	NOW() as last_computation_date
+	FROM
+		business_features features
+			INNER JOIN
+		link_categories_features lcf ON features.id = lcf.feature_id
+			INNER JOIN
+		link_businesses_categories lbc ON lcf.category_id = lbc.category_id
+			INNER JOIN
+		businesses ON businesses.id = lbc.business_id
+	WHERE business.id=NOW.id;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `businesses_comparisons`
@@ -352,87 +385,6 @@ INSERT INTO `countries` VALUES (1,'AF','AFGHANISTAN','Afghanistan','AFG',4,93),(
 UNLOCK TABLES;
 
 --
--- Table structure for table `link_business_category`
---
-
-DROP TABLE IF EXISTS `link_business_category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `link_business_category` (
-  `business_id` mediumint(5) unsigned NOT NULL,
-  `category_id` mediumint(5) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `link_business_category`
---
-
-LOCK TABLES `link_business_category` WRITE;
-/*!40000 ALTER TABLE `link_business_category` DISABLE KEYS */;
-INSERT INTO `link_business_category` VALUES (1,1),(1,5),(1,2),(2,2),(3,1),(4,2);
-/*!40000 ALTER TABLE `link_business_category` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `link_business_tag`
---
-
-DROP TABLE IF EXISTS `link_business_tag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `link_business_tag` (
-  `business_id` int(10) unsigned DEFAULT NULL,
-  `tag_id` int(10) unsigned DEFAULT NULL,
-  `nb_yes` smallint(5) unsigned DEFAULT NULL,
-  `nb_no` smallint(5) unsigned DEFAULT NULL,
-  KEY `tag_id` (`tag_id`),
-  KEY `business_id` (`business_id`),
-  CONSTRAINT `link_business_tag_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `business_tags` (`id`),
-  CONSTRAINT `link_business_tag_ibfk_2` FOREIGN KEY (`business_id`) REFERENCES `businesses` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `link_business_tag`
---
-
-LOCK TABLES `link_business_tag` WRITE;
-/*!40000 ALTER TABLE `link_business_tag` DISABLE KEYS */;
-INSERT INTO `link_business_tag` VALUES (1,7,NULL,NULL),(1,6,NULL,NULL),(2,5,NULL,NULL),(1,1,NULL,NULL);
-/*!40000 ALTER TABLE `link_business_tag` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `link_business_tags`
---
-
-DROP TABLE IF EXISTS `link_business_tags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `link_business_tags` (
-  `business_id` int(10) unsigned DEFAULT NULL,
-  `tag_id` int(10) unsigned DEFAULT NULL,
-  `nb_yes` smallint(5) unsigned DEFAULT NULL,
-  `nb_no` smallint(5) unsigned DEFAULT NULL,
-  KEY `tag_id` (`tag_id`),
-  KEY `business_id` (`business_id`),
-  CONSTRAINT `link_business_tags_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `business_tags` (`id`),
-  CONSTRAINT `link_business_tags_ibfk_2` FOREIGN KEY (`business_id`) REFERENCES `businesses` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `link_business_tags`
---
-
-LOCK TABLES `link_business_tags` WRITE;
-/*!40000 ALTER TABLE `link_business_tags` DISABLE KEYS */;
-INSERT INTO `link_business_tags` VALUES (1,7,NULL,NULL),(1,6,NULL,NULL),(2,5,NULL,NULL),(1,1,NULL,NULL);
-/*!40000 ALTER TABLE `link_business_tags` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `link_businesses_categories`
 --
 
@@ -508,29 +460,6 @@ INSERT INTO `link_categories_features` VALUES (1,7),(1,6),(2,5),(1,1);
 UNLOCK TABLES;
 
 --
--- Table structure for table `link_category_feature`
---
-
-DROP TABLE IF EXISTS `link_category_feature`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `link_category_feature` (
-  `category_id` smallint(5) unsigned NOT NULL,
-  `feature_id` smallint(5) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `link_category_feature`
---
-
-LOCK TABLES `link_category_feature` WRITE;
-/*!40000 ALTER TABLE `link_category_feature` DISABLE KEYS */;
-INSERT INTO `link_category_feature` VALUES (1,7),(1,6),(2,5),(1,1);
-/*!40000 ALTER TABLE `link_category_feature` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `link_comments_images`
 --
 
@@ -556,34 +485,6 @@ LOCK TABLES `link_comments_images` WRITE;
 /*!40000 ALTER TABLE `link_comments_images` DISABLE KEYS */;
 INSERT INTO `link_comments_images` VALUES (1,1);
 /*!40000 ALTER TABLE `link_comments_images` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `link_features_tags`
---
-
-DROP TABLE IF EXISTS `link_features_tags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `link_features_tags` (
-  `feature_id` int(10) unsigned DEFAULT NULL,
-  `tag_id` int(10) unsigned DEFAULT NULL,
-  `pertinence` double DEFAULT NULL,
-  KEY `tag_id` (`tag_id`),
-  KEY `feature_id` (`feature_id`),
-  CONSTRAINT `link_features_tags_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `business_tags` (`id`),
-  CONSTRAINT `link_features_tags_ibfk_2` FOREIGN KEY (`feature_id`) REFERENCES `business_features` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `link_features_tags`
---
-
-LOCK TABLES `link_features_tags` WRITE;
-/*!40000 ALTER TABLE `link_features_tags` DISABLE KEYS */;
-INSERT INTO `link_features_tags` VALUES (2,3,0.001),(3,2,0.002),(1,4,0.003),(4,1,0.004);
-/*!40000 ALTER TABLE `link_features_tags` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -686,7 +587,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `category` (`category`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`category`) REFERENCES `user_categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -695,7 +596,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'monsiteestcool@gg.pd','Paul','Ecoffet','qshfsouih',1,'FR','img/default-user-avatar-small.png',NULL,'2014-05-02 07:55:47','2014-05-10 17:49:40',NULL,NULL),(2,'tagadapwet@hotmail.com','Jean-Baptiste','Klifengerd','qshfsouih',1,'FR','img/default-user-avatar-small.png',NULL,'2014-05-02 07:55:47','2014-05-02 07:55:47',NULL,NULL),(3,'jeanbob@aol.com','Eric','Judor','qshfsouih',1,'FR','img/default-user-avatar-small.png',NULL,'2014-05-02 07:55:47','2014-05-02 07:55:47',NULL,NULL);
+INSERT INTO `users` VALUES (1,'monsiteestcool@gg.pd','Paul','Ecoffet','qshfsouih',1,'FR','img/default-user-avatar-small.png',NULL,'2014-05-02 07:55:47','2014-05-10 17:49:40',NULL,NULL),(2,'tagadapwet@hotmail.com','Jean-Baptiste','Klifengerd','qshfsouih',1,'FR','img/default-user-avatar-small.png',NULL,'2014-05-02 07:55:47','2014-05-02 07:55:47',NULL,NULL),(3,'jeanbob@aol.com','Eric','Judor','qshfsouih',1,'FR','img/default-user-avatar-small.png',NULL,'2014-05-02 07:55:47','2014-05-02 07:55:47',NULL,NULL),(4,'ecoffet.paul@gmail.com','Paul','Ecoffet','92e6bb4ad8b6c7231fa96adf1343d928ffd59c51',1,'FR','img/default-user-avatar-small.png','5370be874dc23','2014-05-12 14:28:55','2014-05-12 14:29:05',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -728,7 +629,7 @@ CREATE TABLE `vm_score_businesses_features` (
 
 LOCK TABLES `vm_score_businesses_features` WRITE;
 /*!40000 ALTER TABLE `vm_score_businesses_features` DISABLE KEYS */;
-INSERT INTO `vm_score_businesses_features` VALUES (1,1,1420,1,1,1,'2014-05-12 13:31:55');
+INSERT INTO `vm_score_businesses_features` VALUES (1,1,1200,3,NULL,NULL,'2014-05-12 14:52:05'),(1,5,1400,2,NULL,NULL,'2014-05-12 14:52:05'),(1,6,1400,4,NULL,NULL,'2014-05-12 14:52:05'),(1,7,1400,4,NULL,NULL,'2014-05-12 14:52:05'),(2,1,1400,2,NULL,NULL,'2014-05-12 14:52:05'),(2,5,1400,1,NULL,NULL,'2014-05-12 14:52:05'),(2,6,1400,3,NULL,NULL,'2014-05-12 14:52:05'),(2,7,1400,3,NULL,NULL,'2014-05-12 14:52:05'),(3,1,100,4,NULL,NULL,'2014-05-12 14:52:05'),(3,6,1400,2,NULL,NULL,'2014-05-12 14:52:05'),(3,7,1400,2,NULL,NULL,'2014-05-12 14:52:05'),(4,1,1400,1,NULL,NULL,'2014-05-12 14:52:05'),(4,6,1400,1,NULL,NULL,'2014-05-12 14:52:05'),(4,7,1400,1,NULL,NULL,'2014-05-12 14:52:05');
 /*!40000 ALTER TABLE `vm_score_businesses_features` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -768,4 +669,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-12 13:35:55
+-- Dump completed on 2014-05-12 15:03:22
